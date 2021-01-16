c8c5ab4a19d60f4c39ff10fe0fd40955f8662310
Ulysse Carion <ulysse@segment.com>
2021-01-16 08:48:29 -0800

    Add company + lorem generators

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+#[macro_export]
 macro_rules! faker_impl_from_file {
     ($name: ident, $file: expr) => {
         impl rand::distributions::Distribution<$name> for rand::distributions::Standard {
@@ -17,6 +18,7 @@ macro_rules! faker_impl_from_file {
     };
 }
 
+#[macro_export]
 macro_rules! faker_impl_from_templates {
     ($name: ident, $($fmt: expr, $($arg:ty),+);+) => {
         impl rand::distributions::Distribution<$name> for rand::distributions::Standard {
@@ -50,54 +52,114 @@ macro_rules! display_impl_for_wrapper {
     };
 }
 
-pub struct AsciiDigit(String);
-faker_impl_from_file!(AsciiDigit, "data/ascii_digit");
+pub mod util {
+    pub struct AsciiDigit(String);
+    faker_impl_from_file!(AsciiDigit, "data/ascii_digit");
 
-pub struct AsciiLowercase(String);
-faker_impl_from_file!(AsciiLowercase, "data/ascii_lowercase");
+    pub struct AsciiLowercase(String);
+    faker_impl_from_file!(AsciiLowercase, "data/ascii_lowercase");
 
-use rand::distributions::{Distribution, Standard};
-use rand::Rng;
-use std::fmt;
-use std::marker::PhantomData;
+    use rand::distributions::{Distribution, Standard};
+    use rand::Rng;
+    use std::fmt;
+    use std::marker::PhantomData;
 
-struct LowerCase<T>(String, PhantomData<T>);
+    pub struct ToAsciiLowercase<T>(String, PhantomData<T>);
 
-impl<T: ToString> Distribution<LowerCase<T>> for Standard
-where
-    Standard: Distribution<T>,
-{
-    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> LowerCase<T> {
-        LowerCase(
-            rng.gen::<T>().to_string().to_lowercase(),
-            std::marker::PhantomData,
-        )
+    impl<T: ToString> Distribution<ToAsciiLowercase<T>> for Standard
+    where
+        Standard: Distribution<T>,
+    {
+        fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> ToAsciiLowercase<T> {
+            let mut s = rng.gen::<T>().to_string().to_lowercase();
+            s.retain(|c| c.is_ascii_lowercase());
+
+            ToAsciiLowercase(s, std::marker::PhantomData)
+        }
+    }
+
+    impl<T: ToString> fmt::Display for ToAsciiLowercase<T> {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            write!(f, "{}", self.0)
+        }
+    }
+
+    pub struct CapitalizeFirstLetter<T>(String, PhantomData<T>);
+
+    impl<T: ToString> Distribution<CapitalizeFirstLetter<T>> for Standard
+    where
+        Standard: Distribution<T>,
+    {
+        fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> CapitalizeFirstLetter<T> {
+            let s = rng.gen::<T>().to_string();
+            let mut c = s.chars();
+
+            CapitalizeFirstLetter(
+                c.next().unwrap().to_uppercase().chain(c).collect(),
+                std::marker::PhantomData,
+            )
+        }
+    }
+
+    impl<T: ToString> fmt::Display for CapitalizeFirstLetter<T> {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            write!(f, "{}", self.0)
+        }
     }
 }
 
-impl<T: ToString> fmt::Display for LowerCase<T> {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self.0)
+pub mod lorem {
+    use crate::util::CapitalizeFirstLetter;
+
+    pub struct Word(String);
+    faker_impl_from_file!(Word, "data/lorem_words");
+
+    struct FirstWord(String);
+    faker_impl_from_templates! {
+        FirstWord,
+
+        "{}", CapitalizeFirstLetter<Word>
     }
-}
 
-struct AsciiLowercaseOnly<T>(String, PhantomData<T>);
+    pub struct Sentence(String);
+    faker_impl_from_templates! {
+        Sentence,
 
-impl<T: ToString> Distribution<AsciiLowercaseOnly<T>> for Standard
-where
-    Standard: Distribution<T>,
-{
-    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> AsciiLowercaseOnly<T> {
-        let mut s = rng.gen::<T>().to_string();
-        s.retain(|c| c.is_ascii_lowercase());
-
-        AsciiLowercaseOnly(s, std::marker::PhantomData)
+        "{} {} {}.", FirstWord, Word, Word;
+        "{} {} {} {}.", FirstWord, Word, Word, Word;
+        "{} {} {} {} {}.", FirstWord, Word, Word, Word, Word;
+        "{} {} {} {} {} {}.", FirstWord, Word, Word, Word, Word, Word;
+        "{} {} {} {} {} {} {}.", FirstWord, Word, Word, Word, Word, Word, Word
     }
-}
 
-impl<T: ToString> fmt::Display for AsciiLowercaseOnly<T> {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self.0)
+    pub struct Paragraph(String);
+    faker_impl_from_templates! {
+        Paragraph,
+
+        "{} {} {}", Sentence, Sentence, Sentence;
+        "{} {} {} {}", Sentence, Sentence, Sentence, Sentence;
+        "{} {} {} {} {}", Sentence, Sentence, Sentence, Sentence, Sentence
+    }
+
+    pub struct Paragraphs(String);
+    faker_impl_from_templates! {
+        Paragraphs,
+
+        "{}\n{}\n{}\n", Paragraph, Paragraph, Paragraph;
+        "{}\n{}\n{}\n{}\n", Paragraph, Paragraph, Paragraph, Paragraph;
+        "{}\n{}\n{}\n{}\n{}\n", Paragraph, Paragraph, Paragraph, Paragraph, Paragraph
+    }
+
+    #[cfg(test)]
+    mod tests {
+        #[test]
+        fn sample_name() {
+            for _ in 0..100 {
+                println!("{}", rand::random::<super::Paragraphs>().to_string());
+            }
+
+            panic!();
+        }
     }
 }
 
@@ -128,8 +190,8 @@ pub mod en_us {
     }
 
     pub mod addresses {
-        use super::super::AsciiDigit;
         use super::names::{FirstName, FullName, LastName};
+        use crate::util::AsciiDigit;
 
         struct CityPrefix(String);
         faker_impl_from_file!(CityPrefix, "data/en_us/city_prefixes");
@@ -205,15 +267,47 @@ pub mod en_us {
         }
     }
 
-    pub mod internet {
-        use super::super::{AsciiDigit, AsciiLowercase, AsciiLowercaseOnly, LowerCase};
+    pub mod company {
         use super::names::{FirstName, LastName};
+
+        struct CompanySuffix(String);
+        faker_impl_from_file!(CompanySuffix, "data/en_us/company_suffixes");
+
+        pub struct CompanyName(String);
+        faker_impl_from_templates! {
+            CompanyName,
+
+            "{} {}", FirstName, CompanySuffix;
+            "{}-{}", LastName, LastName;
+            "{}, {}, and {}", LastName, LastName, LastName
+        }
+
+        struct SloganAdjective(String);
+        faker_impl_from_file!(SloganAdjective, "data/en_us/slogan_adjectives");
+
+        struct SloganDescriptor(String);
+        faker_impl_from_file!(SloganDescriptor, "data/en_us/slogan_descriptors");
+
+        struct SloganNouns(String);
+        faker_impl_from_file!(SloganNouns, "data/en_us/slogan_nouns");
+
+        pub struct Slogan(String);
+        faker_impl_from_templates! {
+            Slogan,
+
+            "{} {} {}", SloganAdjective, SloganDescriptor, SloganNouns
+        }
+    }
+
+    pub mod internet {
+        use super::names::{FirstName, LastName};
+        use crate::util::{AsciiDigit, AsciiLowercase, ToAsciiLowercase};
 
         struct DomainWord(String);
         faker_impl_from_templates! {
             DomainWord,
 
-            "{}", LowerCase<LastName>
+            "{}", ToAsciiLowercase<LastName>
         }
 
         struct DomainTLD(String);
@@ -230,12 +324,10 @@ pub mod en_us {
         faker_impl_from_templates! {
             Username,
 
-            "{}{}", AsciiLowercaseOnly<LowerCase<FirstName>>, AsciiLowercaseOnly<LowerCase<LastName>>;
-            "{}{}{}", AsciiLowercaseOnly<LowerCase<FirstName>>, AsciiLowercaseOnly<LowerCase<LastName>>, AsciiDigit;
-            "{}{}{}{}", AsciiLowercaseOnly<LowerCase<FirstName>>, AsciiLowercaseOnly<LowerCase<LastName>>, AsciiDigit, AsciiDigit;
-            "{}{}", AsciiLowercase, AsciiLowercaseOnly<LowerCase<LastName>>;
-            "{}{}{}", AsciiLowercase, AsciiLowercaseOnly<LowerCase<LastName>>, AsciiDigit;
-            "{}{}{}{}", AsciiLowercase, AsciiLowercaseOnly<LowerCase<LastName>>, AsciiDigit, AsciiDigit
+            "{}{}", AsciiLowercase, ToAsciiLowercase<LastName>;
+            "{}{}{}", AsciiLowercase, ToAsciiLowercase<LastName>, AsciiDigit;
+            "{}{}{}{}", AsciiLowercase, ToAsciiLowercase<LastName>, AsciiDigit, AsciiDigit;
+            "{}{}", ToAsciiLowercase<FirstName>, ToAsciiLowercase<LastName>
         }
 
         pub struct Email(String);
@@ -244,17 +336,16 @@ pub mod en_us {
 
             "{}@{}", Username, Domain
         }
+    }
 
-        #[cfg(test)]
-        mod tests {
-            #[test]
-            fn sample_name() {
-                for _ in 0..100 {
-                    println!("{}", rand::random::<super::Email>().to_string());
-                }
+    pub mod phones {
+        use crate::util::AsciiDigit;
 
-                panic!();
-            }
+        pub struct PhoneNumber(String);
+        faker_impl_from_templates! {
+            PhoneNumber,
+
+            "{}{}{}-{}{}{}-{}{}{}{}", AsciiDigit, AsciiDigit, AsciiDigit, AsciiDigit, AsciiDigit, AsciiDigit, AsciiDigit, AsciiDigit, AsciiDigit, AsciiDigit
         }
     }
 }
